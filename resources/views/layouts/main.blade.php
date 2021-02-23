94181bd8eaf531e292e99a851cab2bd9999b5d9d
Emeka Mbachu <victor.c.mbachu@gmail.com>
2021-02-23 14:48:06 +0100

    added tawk api

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -243,7 +243,7 @@
     (function(){
         var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
         s1.async=true;
-        s1.src='https://embed.tawk.to/601c2e93a9a34e36b973e46b/1etn1bssp';
+        s1.src='https://embed.tawk.to/6035072a9c4f165d47c642bf/1ev7i809m';
         s1.charset='UTF-8';
         s1.setAttribute('crossorigin','*');
         s0.parentNode.insertBefore(s1,s0);
